58a827e2459fe5e8a8921d9d7b00f113ba7577b8
jeelly <khk3159@naver.com>
2022-06-25 13:12:00 +0900

    CI/CD 테스트

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Main = () => {
     return (
-        <div>메인페이지입니다.!!!!!!!</div>
-        <div>시키는대로 했어 난..</div>
+        <>
+            <div>메인페이지입니다.!!!!!!!</div>
+            <div>시키는대로 했어 난..</div>
+            <div>시키는대로 했어 난..</div>
+        </>
     );
 };
 
