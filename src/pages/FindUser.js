5e20b2af848c96773610cad49661b0a0ce0ec159
BO_ORAM <105050618+Bo-oram@users.noreply.github.com>
2022-06-29 14:40:26 +0900

    유저 아이디 비밀번호 찾기 기능 구현

---
+++ src/pages/FindUser.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import FindId from "../components/findUser/FindId";
+import FindPassword from "../components/findUser/FindPassword";
+
+const FindUser = () => {
+    return (
+        <div>
+            <FindId/>
+            <FindPassword/>            
+        </div>
+    )
+
+}
+
+export default FindUser
