8c9198f029044a6d820f7f9747e5eda3c44ecac7
jeelly <khk3159@naver.com>
2022-07-01 03:34:53 +0900

    Mainpage 기능구현 DB데이터 불러오기  #3

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
-import { loadpostsDB } from "../redux/modules/postsSlice";
+import { loadpostDB } from "../redux/modules/postSlice";
 import { useDispatch } from "react-redux";
 
 //component
@@ -12,13 +12,15 @@ import FindUser from "../pages/FindUser";
 import Login from "../pages/Login";
 import PageNotFound from "../pages/PageNotFound";
 import Main from "../pages/Main";
+import ListPage from "../pages/ListPage";
+import Detail from "../pages/Detail";
 
 function App() {
   const dispatch = useDispatch();
   const [isloaded, setIsloaded] = useState(false);
 
   useEffect(() => {
-    dispatch(loadpostsDB(0));
+    dispatch(loadpostDB(0));
     setIsloaded(true);
   }, []);
 
@@ -26,9 +28,12 @@ function App() {
     <AppLayout>
       <Routes>
         <Route path="/" element={isloaded && <Main />} />
+        <Route path="/ListPage" element={isloaded && <ListPage />} />
         <Route path="/signup" element={<Singup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/finduser" element={<FindUser />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/detail/:id" element={<Detail />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </AppLayout>
